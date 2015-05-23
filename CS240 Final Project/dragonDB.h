e36e4ab07ec445895de48ee79c2f070bcf5418d5
Ben Mittelberger <bmittelb@stanford.edu>
2015-05-23 16:13:27 -0700

    started impl of dragon_core

--- CS240 Final Project/dragonDB.h
+++ CS240 Final Project/dragonDB.h
@@ -11,15 +11,26 @@
 #include <queue>
 #include <string>
 #include <iostream>
+#include <time.h>
 
 namespace std{
     
 #ifndef CS240_Final_Project_dragonDB_h
 #define CS240_Final_Project_dragonDB_h
+
+
+class dragon_db;
+class dragon_core;
+class dragon_segment;
+    
+struct segment_entry{
+    string value;
+    uint64_t timestamp;
+};
     
 class dragon_segment {
 private:
-    map<string, string> segment;
+    map<string, segment_entry> segment;
     int version_number;
     mutex segment_lock; //used if consistency is required
     
@@ -34,29 +45,42 @@ public:
 //the data put into another core's queue
 //if not hashed to own segment
 struct package {
-    pair<string,string> key_val; //actual k/v data
+    pair<string,string> contents; //actual k/v data
     uint64_t timestamp;
+};
+
+// struct for each core to place info in
+// another core's mailbox -- lock is only
+// used if strict consistency is turned on
+struct slot {
+    queue< package > packages;
+    mutex mailbox_lock;
 };
 
 class dragon_core {
 private:
-    map<int, dragon_segment*>  map_segments; //hashing out to other segments on diff cores
+    
+    dragon_db* db;
     int core_id;
     int num_cores;
     int num_entries;
+    
     uint64_t disk_flush_last_done; //indicates when we last flushed to disk
     uint64_t mailbox_last_checked; //indicates when we last checked mailboxes
     uint64_t mailbox_rate; //interval between mailbox checking
     bool consistent;
-    vector< queue<transfer_data> > mailboxes;
+    vector<slot> mailbox;
     
+    hash<string> hasher;
     int find_core(string key); //hashes incoming key to find which segment it should go to
     
 public:
     dragon_core(string filename);
-    int put(string key, string value);
+    bool put(string key, string value);
     string get(string key);
     void set_consistency(bool on);
+    void send_package(package p);
+    void deliver_package(int slot_num, package& package);
     
 };
 
@@ -65,11 +89,16 @@ class dragon_db {
 private:
     vector<dragon_core* > cores;
     int num_cores;
-            bool consistent;
+    bool consistent;
+    map<int, dragon_segment*>  map_segments; //hashing out to other segments on diff cores
+    map<int, dragon_core*> map_cores;
     
 public:
     
     uint64_t disk_flush_rate;
+    
+    dragon_segment* get_segment(int core_id);
+    dragon_core* get_core(int core_id);
     
     //load persisted key/val store from disk
     //if filename is empty, init a new k/v store
--- CS240 Final Project/dragon_core.cpp
+++ CS240 Final Project/dragon_core.cpp
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include "dragonDB.h"
+#include <functional>
+
 
 using namespace std;
 
@@ -29,6 +31,55 @@ using namespace std;
  * allows a core to queue data packages destinated for segments belonging to
  * another core.
  */
-dragon_core::dragon_core(string filename){
-    
+dragon_core::dragon_core(string filename) {
+    //TODO: IMPLEMENT THIS FN
 }
+
+
+/* First attempts to put it into our own segment if it hashes to us, 
+ * otherwise, we package it and send it to another thread's mailbox */
+bool dragon_core::put(string key, string value) {
+    int correct_core = find_core(key);
+    if (correct_core == this->core_id){ //if the segment is our own, yay! just write to segment
+        dragon_segment* segment = db->get_segment(correct_core);
+        return segment->put(key,value);
+    } else { //create a package, and send it off to another core to be put in its mailbox
+        pair<string,string> package_contents(key,value);
+        package new_package;
+        new_package.timestamp = time(0);
+        new_package.contents = package_contents;
+        dragon_core* dest_core = db->get_core(correct_core);
+        dest_core->deliver_package(this->core_id,new_package);
+    }
+    //TODO: figure out flushing our own mailbox
+    return true;
+}
+
+
+void dragon_core::deliver_package(int slot_num, package &package){
+    mailbox[slot_num].packages.push(package);
+}
+
+string dragon_core::get(string key){
+    return "";
+}
+
+void dragon_core::set_consistency(bool on) {
+    consistent = on;
+}
+
+/* Utilizes the c++ hash type to hash the string and find the correct
+ * core for this inputed key.
+ *
+ * @param key for finding the correct core
+*/
+int dragon_core::find_core(string key) {
+    return hasher(key)%num_cores;
+    //TODO assuming thread_ids range from 0 to n-1 -- must make sure
+}
+
+
+
+
+
+
