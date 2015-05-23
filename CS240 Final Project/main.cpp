2c725ddd6424a2a0f607f2e667c2fa3194036b0b
Ben Mittelberger <bmittelb@stanford.edu>
2015-05-23 11:30:21 -0700

    Initial Commit

---
+++ CS240 Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS240 Final Project
+//
+//  Created by Ben Mittelberger on 5/23/15.
+//  Copyright (c) 2015 cs240. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
