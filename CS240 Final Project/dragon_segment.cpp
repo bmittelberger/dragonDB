b81def563d0218da80e6aabe73b0d71edda5322e
Suzanne Stathatos <suzanne.stathatos@gmail.com>
2015-05-23 14:18:38 -0700

    added files

--- CS240 Final Project/dragon_segment.cpp
+++ CS240 Final Project/dragon_segment.cpp
@@ -2,9 +2,8 @@
 //  dragon_segment.cpp
 //  CS240 Final Project
 //
+//  Created by Suzanne Stathatos on 5/23/15.
 //  Copyright (c) 2015 cs240. All rights reserved.
 //
 
 #include <stdio.h>
-
-#include "dragonDB.h"
