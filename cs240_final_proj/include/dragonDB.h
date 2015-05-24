d72f730a41e692ce5f40b2fdd04ea72ae876c9d5
Evan Shieh <eshieh@stanford.edu>
2015-05-24 22:28:14 +0000

    Cleaning dragon_core

--- cs240_final_proj/include/dragonDB.h
+++ cs240_final_proj/include/dragonDB.h
@@ -10,7 +10,10 @@
 #include <map>
 #include <vector>
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
 #include <pthread.h>
 #include <sched.h>
 
@@ -115,11 +118,12 @@ private:
     vector<slot> mailbox;
     
     hash<string> hasher;
-    int find_core(string key); //hashes incoming key to find which segment it should go to
+    int map_to_core(string key); //hashes key to find which segment it should go to
     void flush_mailbox();
     
 public:
     dragon_core(string filename, int num_cores);
+	~dragon_core();
     void put(string key, string value);
     string get(string key);
     void set_consistency(bool on);
--- cs240_final_proj/src/dragon_core.cpp
+++ cs240_final_proj/src/dragon_core.cpp
@@ -14,8 +14,8 @@
 using namespace std;
 
 /* Default variable values. */
-const bool CONSISTENCY_MODEL = false;		/* true -> strong, false -> eventual */
-const int MAILBOX_RATE = 500;						/* Mailbox checking frequency, in ms */
+const bool CONSISTENCY_MODEL = false;       /* true -> strong, false -> eventual */
+const int MAILBOX_RATE = 500;               /* Mailbox checking frequency, in ms */
 
 /* Constructor: dragon_core
  * 
@@ -28,49 +28,99 @@ const int MAILBOX_RATE = 500;						/* Mailbox checking frequency, in ms */
  */
 dragon_core::dragon_core(string filename, int num_cores) {
 
-		/* Instantiate private variables. */
+    /* Instantiate private variables. */
     num_entries = 0;
     disk_flush_last_done = time(0);
     mailbox_last_checked = time(0);
     this->num_cores = num_cores;
 
-		/* Set default values for rate, consistency model. */
+    /* Set default values for rate, consistency model. */
     consistent = CONSISTENCY_MODEL;
-		mailbox_rate = MAILBOX_RATE;
+        mailbox_rate = MAILBOX_RATE;
     
-		/* Initialize the mailboxes. */
-    for(int i = 0; i < num_cores; i++ ) {
+    /* Initialize the mailboxes. */
+    for (int i = 0; i < num_cores; i++ ) {
         slot s; // TODO: should this be declared on the heap?
         s.packages = new queue<package>;
         mailbox.push_back(s);
     }
 }
 
-/* First attempts to put it into our own segment if it hashes to us, 
- * otherwise, we package it and send it to another thread's mailbox 
+/* Destructor: ~dragon_core
+ * 
+ * Frees resources associated with the core. 
+ */
+dragon_core::~dragon_core() {
+    for (int i = 0; i < num_cores; i++) {
+        delete mailbox[i].packages;
+    }
+}
+
+/* Function: dragon_core::put
  *
- * @param key as the string key for the k/v
- * @param value as the string value for the k/v
+ * A thread-specific "put" handler called by dragon_db on a particular
+ * dragon_core. This function first hashes the key to determine if the
+ * K/V pair can be processed on this core. If so, the put is performed
+ * immediately - otherwise, this function will package the K/V pair
+ * and send it to another dragon_core to be processed. In the latter
+ * case, the pair will then be processed immediately if and only if
+ * strong consistency is specified as the model.
+ *
+ * @param key as the string key for the K/V
+ * @param value as the string value for the K/V
  */
 void dragon_core::put(string key, string value) {
-    int dest_core_id = find_core(key);
+
+    /* Create a package out of the K/V pair. */
     package p;
     p.contents = pair<string,string>(key,value);
     p.timestamp = time(0);
+
+    /* Determine which core's segment the key belongs on. */
+    int dest_core_id = map_to_core(key);
     
-    if (dest_core_id == this->core_id) { //if the segment is our own, yay! just write to segment
+    /* If the segment is owned locally, perform the put. */
+    if (dest_core_id == this->core_id) { 
         dragon_segment* segment = db->get_segment(dest_core_id);
         segment->put(p);
-    } else { //create a package, and send it off to another core to be put in its mailbox
+
+    /* If the segment is owned by another core, queue the package. */
+    } else { 
         dragon_core* dest_core = db->get_core(dest_core_id);
         dest_core->deliver_package(this->core_id,p);
     }
     
+    /* Flush the local core's mailbox periodically. */
     if (time(0) - mailbox_last_checked > mailbox_rate) {
         flush_mailbox();
     }
 }
 
+/* Function: dragon_core::get
+ *
+ * A thread-specific "get" handler called by dragon_db on a particular
+ * dragon_core. This function locates the segment that the key belongs
+ * on, and retrieves the corresponding value. The empty string is
+ * returned if the entry has not yet been inserted into the store.
+ *
+ * @param key as the string key for the K/V
+ */
+string dragon_core::get(string key) {
+
+    /* Determine which core's segment the key belongs on. */
+    int dest_core = map_to_core(key);
+
+    /* Check to see if an entry exists for this key. */
+    dragon_segment* segment = db->get_segment(dest_core);
+    segment_entry* entry = segment->get(key);
+
+    /* Return entry value if exists, or "" otherwise. */
+    if (!entry) {
+        return "";
+    } else {
+        return entry->value;
+    }
+}
 
 void dragon_core::flush_mailbox() {
     dragon_segment* segment = db->get_segment(core_id);
@@ -102,17 +152,6 @@ void dragon_core::deliver_package(int slot_num, package &package) {
     pthread_mutex_unlock(&mailbox[slot_num].mailbox_lock);
 }
 
-string dragon_core::get(string key) {
-    int dest_core = find_core(key);
-    dragon_segment* segment = db->get_segment(dest_core);
-    segment_entry* entry = segment->get(key);
-    if (!entry) {
-        return "";
-    } else {
-        return entry->value;
-    }
-}
-
 void dragon_core::set_flush_rate(uint64_t rate) {
     mailbox_rate = rate;
 }
@@ -126,10 +165,7 @@ void dragon_core::set_consistency(bool is_strong) {
  *
  * @param key for finding the correct core
 */
-int dragon_core::find_core(string key) {
+int dragon_core::map_to_core(string key) {
     return hasher(key)%num_cores;
     //TODO assuming thread_ids range from 0 to n-1 -- must make sure
 }
-
-
-
