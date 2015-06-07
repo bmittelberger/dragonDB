46bba3a914b6c2685223744034dcd02f278a9672
Suzanne Stathatos <suzanne.stathatos@gmail.com>
2015-06-07 21:11:25 +0000

    added README

--- cs240_final_proj/benchmarks/make_big_puts.sh
+++ cs240_final_proj/benchmarks/make_big_puts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAX=1000
+MAX=100000
 OUTPUT="../tests/big_puts.tst"
 rm $OUTPUT
 echo "open dragon_store" > $OUTPUT
@@ -8,4 +8,4 @@ for i in `seq 1 $MAX`
 do 
 	echo "puts $i $i" >> $OUTPUT  #puts key-val pairs where key=val
 done
-echo "close" >> $OUTPUT
+#echo "close" >> $OUTPUT
